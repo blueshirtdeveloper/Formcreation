d5947416b6215988522bf68f1ab9e5e332799639
Tummala krishna Kishore <Tummalakrishna.Kishore@conduent.com>
2019-10-16 21:25:27 +0530

    Initial Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
-
+import { MY_FORM_MODEL } from './mymodel';
+import { DynamicFormModel, DynamicFormService } from '@ng-dynamic-forms/core';
+import { FormGroup } from '@angular/forms';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +9,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'formapp';
+  formModel: DynamicFormModel = MY_FORM_MODEL;
+  formGroup: FormGroup;
+
+  constructor(private formService: DynamicFormService) {}
+
+  ngOnInit() {
+      this.formGroup = this.formService.createFormGroup(this.formModel);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,21 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { DynamicFormsMaterialUIModule } from '@ng-dynamic-forms/ui-material';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ReactiveFormsModule } from '@angular/forms';
+import { DynamicFormsCoreModule } from '@ng-dynamic-forms/core';
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule,
+    DynamicFormsCoreModule,
+    DynamicFormsMaterialUIModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
